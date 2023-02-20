c6921a102d7e355cd7326057a4acbb52259a9244
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-20 20:56:29 +0000

    m

--- include/test_lh.php
+++ include/test_lh.php
@@ -5,8 +5,8 @@ include_once __DIR__.'/functions.php';
 include_once __DIR__.'/tgdb.php';    
 ?>
 <span style="font-weight: bold;font-size:14px;">SVXReflector Activity</span>
-<fieldset style=" width:620px;box-shadow:0 0 10px #999;background-color:#e8e8e8e8;margin-top:10px;margin-left:0px;margin-right:0px;font-size:12px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
-  <!--<form method="post">
+<!--<fieldset style=" width:620px;box-shadow:0 0 10px #999;background-color:#e8e8e8e8;margin-top:10px;margin-left:0px;margin-right:0px;font-size:12px;border-top-left-radius: 10px; border-top-right-radius: 10px;border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
+  <form method="post">
   <table style="margin-top:3px;">
     <tr height=25px>
       <th width=150px>Time (<?php echo date('T')?>)</th>
