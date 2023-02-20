943c9b2436922bab0fddb04c345cdbbd5bc3ded2
Chris G4NAB_F5VMR <g4nab.ne63@gmail.com>
2023-02-20 19:20:44 +0000

    m

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ else { $callsign="NOCALL";
     <meta name="language" content="English" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="generator" content="SVXLink" />
-    <meta name="Author" content="G4NAB, SP2ONG, SP0DZ" />
-    <meta name="Description" content="Dashboard for SVXLink by G4NAB, SP2ONG, SP0DZ" />
-    <meta name="KeyWords" content="SVXLink,G4NAB, SP2ONG, SP0DZ" />
+    <meta name="Author" content="G4NAB" />
+    <meta name="Description" content="Dashboard for SVXReflector by G4NAB" />
+    <meta name="KeyWords" content="SVXLink,G4NAB" />
     <meta http-equiv="cache-control" content="max-age=0" />
     <meta http-equiv="cache-control" content="no-cache, no-store, must-revalidate" />
     <meta http-equiv="expires" content="0" />
